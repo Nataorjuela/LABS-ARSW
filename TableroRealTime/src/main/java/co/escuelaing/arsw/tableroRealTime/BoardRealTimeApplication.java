7bab430915034c428258e33f86d63d3e62a4f5de
Nataorjuela <nataorjuela62@gmail.com>
2022-07-20 11:21:35 -0500

    readme

---
+++ TableroRealTime/src/main/java/co/escuelaing/arsw/tableroRealTime/BoardRealTimeApplication.java
@@ -0,0 +1,12 @@
+package co.escuelaing.arsw.tableroRealTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardRealTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardRealTimeApplication.class, args);
+    }
+
+}
